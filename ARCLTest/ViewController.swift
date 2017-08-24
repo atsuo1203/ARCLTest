4938b5546c105e59a15f69a06dc8397208d7b6f6
atsuo1203 <atsuo@cps.im.dendai.ac.jp>
2017-08-24 19:43:05 +0900

    任意の住所に画像を出すことができた

--- ARCLTest/ViewController.swift
+++ ARCLTest/ViewController.swift
@@ -10,10 +10,28 @@ import CoreLocation
 import ARCL
 
 class ViewController: UIViewController {
+    
+    var sceneLocationView = SceneLocationView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        sceneLocationView.run()
+        //五反田駅
+        let coordinate = CLLocationCoordinate2D(latitude: 35.626179, longitude: 139.723925)
+        
+        let location = CLLocation(coordinate: coordinate, altitude: 30)
+        let image = UIImage(named: "aichan_aikon")!
+        let annotationNode = LocationAnnotationNode(location: location, image: image)
+        sceneLocationView.addLocationNodeWithConfirmedLocation(locationNode: annotationNode)
+        view.addSubview(sceneLocationView)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        sceneLocationView.frame = view.bounds
     }
 
     override func didReceiveMemoryWarning() {
